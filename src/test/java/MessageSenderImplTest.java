1a8a10bddc36af7c415df2d4607c114e9eb0255d
bulatka <305264@gmail.com>
2022-05-22 11:46:40 +0300

    100% done!

--- src/test/java/MessageSenderImplTest.java
+++ src/test/java/MessageSenderImplTest.java
@@ -6,7 +6,9 @@ import org.mockito.Mockito;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
+import ru.netology.geo.GeoServiceImpl;
 import ru.netology.i18n.LocalizationService;
+import ru.netology.i18n.LocalizationServiceImpl;
 import ru.netology.sender.MessageSender;
 import ru.netology.sender.MessageSenderImpl;
 
@@ -14,41 +16,41 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Stream;
 
-import static ru.netology.geo.GeoServiceImpl.*;
 import static ru.netology.sender.MessageSenderImpl.IP_ADDRESS_HEADER;
 
 public class MessageSenderImplTest {
 
-    static final private Map <String, String> headers = new HashMap<>();
-    static final private String usaIp = "96.95.94.93";
-    static final private String rusIp = "172.95.94.93";
-    static final private String rus = "Добро пожаловать";
-    static final private String defaultMessage = "Welcome";
+    static final private Map<String, String> HEADERS = new HashMap<>();
+    static final private String USA_IP = "96.95.94.93";
+    static final private String RUS_IP = "172.95.94.93";
+    static final private String RUS = "Добро пожаловать";
+    static final private String DEFAULT_MESSAGE = "Welcome";
+    static final private String MOCK_STR = "";
+    static final private int MOCK_INT = 111;
 
 
-    LocalizationService locService = Mockito.mock(LocalizationService.class);
+    LocalizationService locService = Mockito.mock(LocalizationServiceImpl.class);
 
-    GeoService geoService = Mockito.mock(GeoService.class);
+    GeoService geoService = Mockito.mock(GeoServiceImpl.class);
 
     @ParameterizedTest
     @MethodSource("methodSource")
-    void testSend(String ip, String message) {
-        headers.put(ip, IP_ADDRESS_HEADER);
+    void testSend(String ip, String message, Country country) {
+        HEADERS.put(IP_ADDRESS_HEADER, ip);
         MessageSender messageSender = new MessageSenderImpl(geoService, locService);
-        Mockito.when(geoService.byIp(ip)).thenReturn(
-                        new Location(
-                        Mockito.anyString(),
-                        country,
-                        Mockito.anyString(), Mockito.anyInt()));
-
-        String actual = messageSender.send(headers);
+        Mockito.when(geoService.byIp(Mockito.anyString())).thenReturn(
+                new Location(
+                        MOCK_STR,
+                        country, MOCK_STR, MOCK_INT));
+        Mockito.when(locService.locale(country)).thenCallRealMethod();
+        String actual = messageSender.send(HEADERS);
         Assertions.assertEquals(message, actual);
     }
 
     public static Stream<Arguments> methodSource() {
         return Stream.of(
-                Arguments.of(rusIp, rus),
-                Arguments.of(usaIp, defaultMessage)
+                Arguments.of(RUS_IP, RUS, Country.RUSSIA),
+                Arguments.of(USA_IP, DEFAULT_MESSAGE, Country.USA)
         );
     }
 }
